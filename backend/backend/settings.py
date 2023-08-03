0241d638d1dd662492a3c65fc0dc3dc4b2900c27
Barbiegit <tostixatovi@gmail.com>
2023-08-03 23:05:50 +0300

    nginx

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB", "django"),
         "USER": os.getenv("POSTGRES_USER", "django_user"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
-        "HOST": os.getenv("DB_HOST", ""),
+        "HOST": os.getenv("DB_HOST", "db"),
         "PORT": os.getenv("DB_PORT", 5432),
     }
 }
